811c9f417185a28e92e4d8de4b8ea6a7e0eeae5b
xtreme6007 <prestonnichols96@gmail.com>
2021-01-05 12:56:52 -0600

    working on code to convert minutes

---
+++ src/MinYTDCalc.java
@@ -0,0 +1,14 @@
+public class MinYTDCalc {
+
+    public static void printYearsAndDays(long minutes){
+        if(minutes < 0) {
+            System.out.println("Invalid Value");
+        }
+        int hours =(int) minutes / 60;
+        int days = hours / 24;
+        int years = days /365;
+        int remainingDays = days % 365;
+        System.out.println(minutes + " min = " + years + " y" + remainingDays + " d");
+
+    }
+}
